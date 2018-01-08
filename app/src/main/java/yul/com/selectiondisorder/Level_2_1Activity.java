2f1d344161d8a1374377230fe3ad68d0969a2ec2
961108-pc\Administrator <coflquf1@naver.com>
2018-01-08 18:15:52 +0900

    make javafiles

---
+++ app/src/main/java/yul/com/selectiondisorder/Level_2_1Activity.java
@@ -0,0 +1,13 @@
+package yul.com.selectiondisorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Level_2_1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_2_1);
+    }
+}
---
+++ app/src/main/java/yul/com/selectiondisorder/Level_3_5Activity.java
@@ -0,0 +1,13 @@
+package yul.com.selectiondisorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Level_3_5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_3_5);
+    }
+}
