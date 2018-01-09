53d905922c66b31c517f9dbc73011359096bf065
961108-pc\Administrator <coflquf1@naver.com>
2018-01-09 20:59:16 +0900

    how to make intent and use startActivity(intnet)
    setOnclickListener 2ways

--- app/src/main/java/yul/com/selectiondisorder/MainActivity.java
+++ app/src/main/java/yul/com/selectiondisorder/MainActivity.java
@@ -1,7 +1,10 @@
 package yul.com.selectiondisorder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button2);
+
+//        View.OnClickListener listner = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(this, Level_1_1Activity.class);
+//                startActivity(intent);
+//            }
+//        };
+//
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(this, Level_1_1Activity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
     }
 }
