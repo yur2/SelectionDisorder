09476ea0e347dd80ba59671f8b948d26d5ea156c
kyuwankim <lpnids@gmail.com>
2018-01-10 20:47:20 +0900

    Add Level1_2 Activity JAVACODE

--- app/src/main/java/yul/com/selectiondisorder/Level1_2Activity.java
+++ app/src/main/java/yul/com/selectiondisorder/Level1_2Activity.java
@@ -1,7 +1,10 @@
 package yul.com.selectiondisorder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Level1_2Activity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class Level1_2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1_2);
+
+        Button btn_level1_2_left = (Button) findViewById(R.id.btn_level1_2_left);
+        Button btn_level1_2_right = (Button) findViewById(R.id.btn_level1_2_right);
+
+        btn_level1_2_left.setText("블랙러시안");
+        btn_level1_2_right.setText("피치크러쉬");
+
+        btn_level1_2_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Level1_2Activity.this, "블랙러시안", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_level1_2_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Level1_2Activity.this, "피치크러쉬", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
