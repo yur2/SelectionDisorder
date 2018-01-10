3b8507ff48b73005b8bfbb0d43da4a096556a04f
kyuwankim <lpnids@gmail.com>
2018-01-10 20:30:04 +0900

    Add Level1_2 Activity

--- app/src/main/java/yul/com/selectiondisorder/MainActivity.java
+++ app/src/main/java/yul/com/selectiondisorder/MainActivity.java
@@ -1,10 +1,10 @@
 package yul.com.selectiondisorder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         btn_left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, Level1_1Activity.class);
+                startActivity(intent);
 
             }
         });
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                
+
             }
         });
 
