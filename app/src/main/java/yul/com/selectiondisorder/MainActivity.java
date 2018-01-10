359d8c8bb6c5bd463488dc209894bba7a5c5c869
kyuwankim <lpnids@gmail.com>
2018-01-10 19:54:56 +0900

    Delete Activities

--- app/src/main/java/yul/com/selectiondisorder/MainActivity.java
+++ app/src/main/java/yul/com/selectiondisorder/MainActivity.java
@@ -1,10 +1,7 @@
 package yul.com.selectiondisorder;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,28 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button2);
-
-//        View.OnClickListener listener = new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(this, Level_1_1Activity.class);
-//                startActivity(intent);
-//            }
-//        };
-//      button.setOnClickListener(listener);
-//
-
-
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(this, Level_1_1Activity.class);
-//                startActivity(intent);
-//            }
-//        });
 
 
     }
