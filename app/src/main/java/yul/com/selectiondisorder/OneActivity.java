971492a085101870ecda4444c8358b70afdd4c77
kyuwankim <lpnids@gmail.com>
2018-01-14 20:42:32 +0900

    complete oneActivity Layout

--- app/src/main/java/yul/com/selectiondisorder/OneActivity.java
+++ app/src/main/java/yul/com/selectiondisorder/OneActivity.java
@@ -1,13 +1,29 @@
 package yul.com.selectiondisorder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class OneActivity extends AppCompatActivity {
+
+    ArrayList<String> datas = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        dataLoad();
+
+
+    }
+
+    private void dataLoad() {
+
+        for(int i = 0 ; i < 10 ; i++){
+            datas.add(i + "");
+        }
+
     }
 }
