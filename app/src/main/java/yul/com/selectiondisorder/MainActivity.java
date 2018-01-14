8daf915e3e444ea0b5d09090593a846ee06fc8f5
kyuwankim <lpnids@gmail.com>
2018-01-14 18:09:46 +0900

    Add One, Two Activities

--- app/src/main/java/yul/com/selectiondisorder/MainActivity.java
+++ app/src/main/java/yul/com/selectiondisorder/MainActivity.java
@@ -8,10 +8,32 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent = null;
+    Button btn_select_one;
+    Button btn_select_two;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn_select_one = (Button) findViewById(R.id.btn_select_one);
+        btn_select_two = (Button) findViewById(R.id.btn_select_two);
+
+        btn_select_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_select_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
