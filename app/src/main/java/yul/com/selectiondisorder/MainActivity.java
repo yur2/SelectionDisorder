942c27e9142f896971a5902999cc02daf4c6fbcb
kyuwankim <lpnids@gmail.com>
2018-01-18 20:44:59 +0900

    complete Widget

--- app/src/main/java/yul/com/selectiondisorder/MainActivity.java
+++ app/src/main/java/yul/com/selectiondisorder/MainActivity.java
@@ -1,59 +1,143 @@
 package yul.com.selectiondisorder;
 
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // 위젯 선언
+    RadioGroup radioGroup;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
+    RadioButton radioButton4;
+    CheckBox checkBox;
+    Switch aSwitch;
+    SeekBar seekBar;
+    ImageView imageView;
+    Button button;
+    TextView textView;
+
+    boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_left = (Button) findViewById(R.id.btn_left);
-        Button btn_right = (Button) findViewById(R.id.btn_right);
+        flag = true;
 
-        btn_left.setText("배불러!");
-        btn_right.setText("배고파 ㅜㅜ");
+        // 위젯 바인딩
+        radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+        radioButton1 = (RadioButton) findViewById(R.id.radioButton);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
+        radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
+        radioButton4 = (RadioButton) findViewById(R.id.radioButton4);
+        checkBox = (CheckBox) findViewById(R.id.checkBox2);
+        aSwitch = (Switch) findViewById(R.id.switch1);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView3);
 
-        btn_left.setOnClickListener(new View.OnClickListener() {
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LeveliamfullActivity.class);
-                startActivity(intent);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioButton:
+                        Toast.makeText(MainActivity.this, "radioButton", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.radioButton2:
+                        Toast.makeText(MainActivity.this, "radioButton2", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.radioButton3:
+                        Toast.makeText(MainActivity.this, "radioButton3", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.radioButton4:
+                        Toast.makeText(MainActivity.this, "radioButton4", Toast.LENGTH_SHORT).show();
+
+                        break;
+                }
             }
         });
 
-        btn_right.setOnClickListener(new View.OnClickListener() {
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Level1_3Activity.class);
-                startActivity(intent);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    Toast.makeText(MainActivity.this, "checkBox ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "checkBox OFF", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-//        View.OnClickListener listener = new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(this, Level_1_1Activity.class);
-//                startActivity(intent);
-//            }
-//        };
-//      button.setOnClickListener(listener);
-//
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    Toast.makeText(MainActivity.this, "Switch ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Switch Off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(this, Level_1_1Activity.class);
-//                startActivity(intent);
-//            }
-//        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "button", Toast.LENGTH_SHORT).show();
+
+                if(flag == true){
+                    imageView.setImageDrawable(getDrawable(R.drawable.ic_launcher_background));
+                    flag = false;
+                }
+                else if(flag == false){
+                    imageView.setImageDrawable(getDrawable(R.drawable.ic_launcher_foreground));
+                    flag = true;
+                }
+
+            }
+        });
 
 
     }
